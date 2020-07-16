31bd77a0b759b811f59eb2286e8510469c228333
chris <root@chris.combobreaker.xtralife.me-ams3>
2020-07-16 10:22:22 +0000

    let our parent (http or dashboard) decide on the default metrics name prefix

--- src/lib/metrics.js
+++ src/lib/metrics.js
@@ -1,8 +1,5 @@
 const os = require('os')
 const client = require('prom-client');
-const collectDefaultMetrics = client.collectDefaultMetrics;
-const prefix = 'xl_backend_';
-collectDefaultMetrics({ prefix });
 
 client.register.setDefaultLabels( { hostname: os.hostname() })
 
