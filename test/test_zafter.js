176c0b2425ceed61baaddb06573a3231d1193feb
Sebastien Didierdefresse <sebastien.xtralife@gmail.com>
2023-02-02 14:01:49 +0100

    Cleaner tests disposal + Add --exit option to mocha test to ensure the process won't keep being stuck after all tests are done

---
+++ test/test_zafter.js
@@ -0,0 +1,18 @@
+/*
+ * This file is named "test_zafter" just to be the last one to be executed after all other tests,
+ * because its purpose is to disconnect the Redis instance(s) used by the other tests
+ */
+global.xlenv = require("xtralife-env");
+
+describe('After tests disposal', function () {
+	return it('should close all running instances', function () {
+		xlenv.inject(['=redisClient', '=redisChannel', '=mongoCx', '=elastic'], function (err, rcl, rch, mongoCx, elastic) {
+			xlenv.broker.stop(); // TimeoutBroker must be stopped (to stop checking for timeouts)
+			// Redis connections must be stopped in order to prevent from the test to keep stuck
+			rcl.disconnect();
+			rch.disconnect();
+			mongoCx.close();
+			elastic.close();
+		});
+	});
+});
